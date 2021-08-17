e96a734128790bf8732d37acd2aeed055ce0ee4d
Sammwy <sammwy.dev@gmail.com>
2021-08-17 14:08:39 -0300

    Added validation in CreateAccountDto

--- src/modules/account/dto/create-account.dto.ts
+++ src/modules/account/dto/create-account.dto.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEmail, Length } from 'class-validator';
 
 @InputType()
 export class CreateAccountDto {
   @Field(() => String)
+  @IsEmail()
   email: string;
 
   @Field(() => String)
+  @Length(8, 256)
   password: string;
 }
