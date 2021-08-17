9faccac0a86475249e887710c48fc37ed9d0140a
Sammwy <sammwy.dev@gmail.com>
2021-08-17 14:15:32 -0300

    Added validation to create-profile dto

--- src/modules/profile/dto/create-profile.dto.ts
+++ src/modules/profile/dto/create-profile.dto.ts
@@ -1,13 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Length } from 'class-validator';
 
 @InputType()
 export class CreateProfileDto {
   @Field(() => String)
+  @Length(1, 32)
   username: string;
 
   @Field(() => String)
+  @Length(1, 64)
   displayName?: string;
 
   @Field(() => String)
+  @Length(0, 256)
   bio?: string;
 }
