871a82a407771d007fb1f725f13a80d83dcf3014
Sammwy <sammwy.dev@gmail.com>
2021-08-17 02:58:40 -0300

    Added MongoDB module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    /**
+     * Load environment variables from the .env file in the root of the project.
+     */
+    ConfigModule.forRoot(),
+
+    /**
+     * Using the database module it will connect to the
+     * mongodb server specified in the environment variable "MONGO_URI"
+     */
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
